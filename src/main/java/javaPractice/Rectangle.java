d93205c0a786f898992203bcdb1b9b01e5d015b7
Srini0216 <srini.pigilam@gmail.com>
2020-02-29 16:13:45 -0500

    rectangle class

---
+++ src/main/java/javaPractice/Rectangle.java
@@ -0,0 +1,45 @@
+package javaPractice;
+
+public class Rectangle {
+	int length = 0;
+	int breadth = 0;
+	int area = 0;
+	
+	Rectangle() {
+		
+	}
+	
+	Rectangle(int lng, int brdt) {
+		length = lng;
+		breadth = brdt;
+		
+	}
+	
+	Rectangle(int lng) {
+		length = lng;
+		breadth = lng;
+		
+	}
+	
+	public int area() {
+		area = length * breadth;
+		System.out.println("Area of the rectangle:" + area);
+		return area;
+	}
+
+	public static void main(String[] args) {
+		// create an object with default constructor
+		Rectangle r1 = new Rectangle();
+		r1.area();
+		
+		// create an object with with one argument to constructor
+		Rectangle r2 = new Rectangle(10);
+		r2.area();
+		
+		// create an object with two arguments constructor
+		Rectangle r3 = new Rectangle(20, 7);
+		r3.area();
+
+	}
+
+}
