c5dcb740dba9131995ff3e5a0bd381ede453c36f
Srini0216 <srini.pigilam@gmail.com>
2020-03-03 20:33:06 -0500

    Attendance

--- src/main/java/javaPractice/Attendance.java
+++ src/main/java/javaPractice/Attendance.java
@@ -14,20 +14,21 @@ double no_of_classes_held;
 double no_of_classes_attended;
 double percentage_of_classes_attended;
 double percentage_cut_off = 75.0;
+String medical_cause;
 
-public void calc_percentage(double held, double attended) {
+public void calc_percentage(double held, double attended, String val) {
 	no_of_classes_held = held;
 	no_of_classes_attended = attended;
+	medical_cause = val;
 	System.out.println(no_of_classes_held);
 	System.out.println(no_of_classes_attended);
 	percentage_of_classes_attended = (no_of_classes_attended / no_of_classes_held) * 100;
 	System.out.println("Attendance: " + percentage_of_classes_attended);
 	
-	if (percentage_of_classes_attended < percentage_cut_off) {
+	if (percentage_of_classes_attended < percentage_cut_off && medical_cause != "Y") {
 		System.out.println("Shortage of attendance, you are not allowed to sit in exam");
 	} else
 		System.out.println("You have attendance required, you can sit in exam");
-	
 }
 	
 	
@@ -37,8 +38,11 @@ public void calc_percentage(double held, double attended) {
 		double num1 = s1.nextDouble();
 		System.out.println("Enter Number of Classes Attended:");
 		double num2 = s1.nextDouble();
+		System.out.println("Do you have a medical cause: Y/N");
+		String medical = s1.next();
+		System.out.println("Medical value: " + medical);
 		Attendance att = new Attendance();
-		att.calc_percentage(num1, num2);
+		att.calc_percentage(num1, num2, medical);
 		
 
 	}
