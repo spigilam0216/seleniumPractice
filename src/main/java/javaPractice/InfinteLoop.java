836352b590ef350df59fdbbbafda4dfb1dce0c66
Srini0216 <srini.pigilam@gmail.com>
2020-03-06 20:21:31 -0500

    Infinite

---
+++ src/main/java/javaPractice/InfinteLoop.java
@@ -0,0 +1,21 @@
+package javaPractice;
+
+public class InfinteLoop {
+
+	public void infinite() {
+
+      int reminder=0;
+      int i = 0;
+	while (i<=reminder) {
+    	  System.out.println("Infinite Loop");
+    
+      }
+      }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		InfinteLoop inf = new InfinteLoop();
+		inf.infinite();
+
+	}
+
+}
